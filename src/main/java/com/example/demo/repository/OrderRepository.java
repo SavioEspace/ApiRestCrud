5c15227f9bb8592e2d1ddb4914250841f6de1b00
Sávio B. <contatosaviosb@gmail.com>
2025-06-18 13:44:18 -0300

    Update OrderRepository.java

--- src/main/java/com/example/demo/repository/OrderRepository.java
+++ src/main/java/com/example/demo/repository/OrderRepository.java
@@ -1,7 +1,11 @@
+// OrderRepository.java
 package com.example.demo.repository;
 
 import com.example.demo.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repositório de acesso a dados para Pedido.
+ */
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
